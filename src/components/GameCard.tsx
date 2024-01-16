8c2fb7af038c4347bd0ce6866b63147ddf708c61
DaraKTCT1 <renchandarat@gmail.com>
2024-01-16 12:32:25 +0700

    show loading skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,9 +7,9 @@ import getCroppedImageUrl from "../services/image-url";
 interface PropType {
   game: Game;
 }
-const GameCard = ({ game }: PropType) => {
+const GameCard = ({ game }: PropType) => {  
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width={"300px"} borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
