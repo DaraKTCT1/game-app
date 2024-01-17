5c2813708c509fdfe0ae7213f85c9387063100df
DaraKTCT1 <renchandarat@gmail.com>
2024-01-17 19:57:32 +0700

    final

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,21 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import like from "../assets/like.png"
+import reallylike from "../assets/reallylike.png"
+import unlike from "../assets/unlike.png"
+
+interface PropType{
+    rating: number;
+}
+const Emoji = ({rating}: PropType) => {
+    if(rating<3) return null;
+    const emojiMap: {[key: number]: ImageProps} = {
+        3: { src: reallylike,alt: "reallylike",boxSize: "30px"},
+        4: { src: like,alt: "like",boxSize: "30px"},
+        5: { src: unlike,alt: "unlike",boxSize: "30px"},
+    }
+  return (
+    <Image {...emojiMap[rating] } marginTop={1} />
+  )
+}
+
+export default Emoji
